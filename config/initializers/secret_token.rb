d94c0b484b40db30bb99bcefc7dfb2a393f5dc96
Raghuram Pulijala <raghuram.pulijala@gmail.com>
2013-11-12 10:54:41 -0500

    Created the website framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetme::Application.config.secret_key_base = '17469112edf18122247e9221673dd4dc9987cb3be44a05924791752298c7a353a254b4a999ea8793a168448bc8d5063622775784a06e4bdc12e1fb695ff355f1'
