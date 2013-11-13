ad61799ab9331d45dbbf45124ea84f40dbea2c1b
Raghuram Pulijala <raghuram.pulijala@gmail.com>
2013-11-13 16:32:08 -0500

    dropped DB

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Meetme::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
 end
